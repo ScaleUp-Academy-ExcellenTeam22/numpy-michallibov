ba6b483210d5c48cf4e4886fd3932f42e5b656f3
michallibov <92799624+michallibov@users.noreply.github.com>
2022-06-09 19:36:36 +0300

    Update third_ex.py

--- third_ex.py
+++ third_ex.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 
-def create_and_print_number_of_rows_and_cols() -> None:
+def create_and_print_number_of_rows_and_cols(given_array) -> None:
     """
     This function creates an array and prints the amount of rows and
     columns that this array has.
+    :param given_array: A given array from the main which we will check it's size.
     """
-    my_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    my_array = given_array
     number_of_rows, number_of_columns = my_array.shape
 
     print("Rows = ", number_of_rows)
@@ -14,4 +15,4 @@ def create_and_print_number_of_rows_and_cols() -> None:
 
 
 if __name__ == '__main__':
-    create_and_print_number_of_rows_and_cols()
+    create_and_print_number_of_rows_and_cols(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
