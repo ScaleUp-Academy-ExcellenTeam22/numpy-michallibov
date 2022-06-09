0a8d3fa66ca8c339052ccb72620938594060d8a6
michallibov <92799624+michallibov@users.noreply.github.com>
2022-06-09 20:10:01 +0300

    Update seventh_ex.py

--- seventh_ex.py
+++ seventh_ex.py
@@ -1,22 +1,22 @@
 import numpy as np
 
 
-def swap_first_and_last_rows(randomized_array) -> None:
+def print_swapped_first_and_last_rows(randomized_array) -> None:
     """
-    This function gets an array and swaps it's first rows with it's last rows.
+    This function gets an array and swaps it's first rows with it's last rows. It prints
+    the swapped array.
     :param randomized_array: the array that we will swap it's first and last rows.
     """
-    swapped_array = randomized_array[::-1]
+    print(randomized_array[::-1])
 
 
-def randomize_array() -> None:
+def randomize_array() -> np.array:
     """
-    This function creates a random array in the size of 4x4. It then calls the function
-    swap_first_and_last_rows to swap the first rows with the last rows.
+    This function creates a random array in the size of 4x4.
+    :return: the randomized array.
     """
-    randomized_array = np.random.rand(4, 4)
-    swap_first_and_last_rows(randomized_array)
+    return np.random.rand(4, 4)
 
 
 if __name__ == '__main__':
-    randomize_array()
+    print_swapped_first_and_last_rows(randomize_array())
